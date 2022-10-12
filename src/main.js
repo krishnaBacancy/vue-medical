05c94d8e1d4ecced278352fded9a6cdb8fa0e0aa
Jimit-bacancy <jimit.shah@bacancy.com>
2022-10-12 18:41:35 +0530

    Added vue-toastification.

--- src/main.js
+++ src/main.js
@@ -4,13 +4,20 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
+import Toast from "vue-toastification";
+import "vue-toastification/dist/index.css";
 
 Vue.config.productionTip = false;
+Vue.use(Toast, {});
 
-// let email = localStorage.getItem("email");
-
-// if (email) {
-//   store.dispatch("users/loginUser", { email, password });
+// let localRoleId = localStorage.role_id;
+// let cookieExists = localRoleId !== undefined && localRoleId !== null;
+// if (cookieExists) {
+//   const roleId = localStorage.getItem("role_id");
+//   const roleIdExists = roleId !== undefined && roleId !== null;
+//   if (roleIdExists) {
+//     store.dispatch("users/loginUser", { roleId });
+//   }
 // }
 
 new Vue({
