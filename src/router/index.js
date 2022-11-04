c4ebf190a9242ca04acec3bb580947ebd50f066e
Jimit-bacancy <jimit.shah@bacancy.com>
2022-11-04 20:26:38 +0530

    Added settings page and 1 route nav guard.

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import HomeView from "../views/dashboard/HomeView.vue";
-import store from "../store/index";
+// import store from "../store/index";
 
 Vue.use(VueRouter);
 
@@ -20,6 +20,7 @@ const routes = [
     path: "/login",
     name: "userlogin",
     component: () => import("../views/user/UserLogin.vue"),
+    meta: { guest: true },
   },
   {
     path: "/user-management",
@@ -42,6 +43,11 @@ const routes = [
     component: () => import("../views/patients/PatientProfile.vue"),
   },
   {
+    path: "/settings",
+    name: "settings",
+    component: () => import("../views/settings/SettingsView.vue"),
+  },
+  {
     path: "*",
     redirect: "/",
   },
@@ -54,14 +60,54 @@ const router = new VueRouter({
 });
 
 router.beforeEach((to, from, next) => {
-  if (to.name !== "userlogin" && store.getters["users/isLoggedIn"]) {
-    next({
-      path: "login",
-      replace: true,
-    });
-  } else {
-    next();
+  // redirect to login page if not logged in and trying to access a restricted page
+  const publicPages = ["/login"];
+  const authRequired = !publicPages.includes(to.path);
+  const loggedIn = localStorage.getItem("user_id");
+
+  if (authRequired && !loggedIn) {
+    return next("/login");
   }
+
+  next();
 });
+
+// router.beforeEach((to, from, next) => {
+//   console.log("To--", to);
+//   console.log("Store--", store);
+//   if (to.name !== "userlogin" && store.getters["users/isLoggedIn"]) {
+//     next({
+//       path: "login",
+//       replace: true,
+//     });
+//   } else {
+//     next();
+//   }
+// });
+
+// router.beforeEach((to, from, next) => {
+//   console.log("To--", to);
+//   if (to.matched.some((record) => record.meta.requiresAuth)) {
+//     if (store.getters["users/isLoggedIn"]) {
+//       next();
+//       return;
+//     }
+//     next("/login");
+//   } else {
+//     next();
+//   }
+// });
+
+// router.beforeEach((to, from, next) => {
+//   if (to.matched.some((record) => record.meta.guest)) {
+//     if (store.getters["users/isLoggedIn"]) {
+//       next("/");
+//       return;
+//     }
+//     next();
+//   } else {
+//     next();
+//   }
+// });
 
 export default router;
