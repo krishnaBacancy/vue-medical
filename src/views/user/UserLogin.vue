d48152c16d758215816f82fce70cf2a920e81b0b
Jimit-bacancy <jimit.shah@bacancy.com>
2022-12-06 21:33:42 +0530

    Login Page recreation and images added.

--- src/views/user/UserLogin.vue
+++ src/views/user/UserLogin.vue
@@ -1,86 +1,147 @@
 <template>
-  <v-app>
+  <v-app class="overflow-hidden">
     <v-main>
-      <v-container class="fill-height mt-3" fluid style="height: 100vh">
-        <v-row justify="center">
-          <v-col cols="12" sm="8" md="8" lg="10">
-            <v-card class="elevation-12">
-              <v-row>
-                <v-col cols="12" md="8">
-                  <v-card-text class="mt-12">
-                    <v-flex
-                      d-flex
-                      xs12
-                      sm12
-                      md12
-                      lg12
-                      style="align-items: flex-start"
-                      class="ml-lg-16"
-                    >
-                      <h1
-                        class="display-2 h1__div text-sm-h4 text-lg-h3 teal--text text--accent-3"
-                        style="text-align: start"
-                      >
-                        Sneak Peek Into Health
-                      </h1>
-                    </v-flex>
-                    <h5 class="text-center mt-3 teal--text">
-                      Login into your account
-                    </h5>
-                    <form class="mt-4" ref="form" @submit.prevent="login">
-                      <v-text-field
-                        prepend-icon="mdi-email"
-                        v-model="email"
-                        name="email"
-                        label="Email"
-                        type="email"
-                        :rules="emailRules"
-                        placeholder="email"
-                        required
-                      ></v-text-field>
+      <v-container class="fill-height mt-3" fluid>
+        <v-row class="white--text">
+          <v-col cols="12" sm="6" md="4" class="ml-10">
+            <v-img
+              class="ml-auto"
+              src="@/assets/arrow1.svg"
+              height="100"
+              width="100"
+              contain
+            ></v-img>
 
-                      <v-text-field
-                        prepend-icon="mdi-lock"
-                        :append-icon="showPassIcon ? 'mdi-eye' : 'mdi-eye-off'"
-                        :type="showPassIcon ? 'text' : 'password'"
-                        v-model="password"
-                        name="password"
-                        label="Password"
-                        hint="At least 6 characters"
-                        placeholder="password"
-                        required
-                        @click:append="showPassIcon = !showPassIcon"
-                      ></v-text-field>
-                      <div class="text-center mt-3 mb-3">
-                        <v-btn rounded color="teal accent-3" type="submit" dark
-                          >SIGN IN</v-btn
-                        >
-                      </div>
-                    </form>
-                  </v-card-text>
+            <div class="text-start mt-n5">
+              <v-flex d-flex md12>
+                <h1>Sneak Peek Into Health</h1>
+              </v-flex>
+              <v-flex d-flex md8>
+                <p class="mt-5">
+                  Get access to essential health data that aids in taking
+                  crucial decisions for life. Track vital information in a
+                  graphical view and hasten the next step.
+                </p>
+              </v-flex>
+              <v-row class="mt-5">
+                <v-col>
+                  <v-img
+                    src="@/assets/Rectangle 112.svg"
+                    contain
+                    height="80"
+                    width="80"
+                  >
+                    <v-img
+                      src="@/assets/Heart hand.png"
+                      height="80"
+                      width="80"
+                      contain
+                    ></v-img>
+                  </v-img>
                 </v-col>
-                <v-col
-                  cols="12"
-                  md="4"
-                  class="accent-3 mt-sm-3 mt-md-0"
-                  style="background-color: #2c3e50"
-                >
-                  <v-card-text class="white--text mt-12">
-                    <h1 class="text-center display-1 text-lg-h3 text-md-h5">
-                      Welcome
-                    </h1>
-                    <h5 class="text-center mt-4 text-body-2">
-                      Get access to essential health data that aids in taking
-                      crucial decisions for life. Track vital information in a
-                      graphical view and hasten the next step.
-                    </h5>
-                  </v-card-text>
-                  <h5 class="white--text mt-4">
-                    © Copyright 2022 Bacancy. All Right Reserved.
-                  </h5>
+                <v-col class="ml-n10">
+                  <v-img
+                    src="@/assets/Rectangle 112.svg"
+                    contain
+                    height="80"
+                    width="80"
+                  >
+                    <v-img
+                      src="@/assets/Heart hand1.png"
+                      height="80"
+                      width="80"
+                      contain
+                    ></v-img>
+                  </v-img>
+                </v-col>
+                <v-col class="ml-n10">
+                  <v-img
+                    src="@/assets/Rectangle 112.svg"
+                    contain
+                    height="80"
+                    width="80"
+                  >
+                    <v-img
+                      src="@/assets/watch.png"
+                      height="80"
+                      width="80"
+                      contain
+                    ></v-img>
+                  </v-img>
                 </v-col>
               </v-row>
-            </v-card>
+            </div>
+          </v-col>
+
+          <v-col cols="12" sm="6" md="3" v-if="!$vuetify.breakpoint.smAndDown">
+            <v-img src="@/assets/login-bg.svg" height="700" width="700" contain>
+              <v-img
+                src="@/assets/Mask Group 7.png"
+                height="700"
+                width="700"
+                contain
+              ></v-img>
+            </v-img>
+          </v-col>
+
+          <v-col cols="12" sm="6" md="4" class="ml-10">
+            <v-img src="@/assets/mobile-screen.svg" height="700" width="600">
+              <v-img
+                class="ml-auto mr-auto"
+                src="@/assets/acculive-main.svg"
+                height="200"
+                width="300"
+                contain
+              ></v-img>
+
+              <div class="white--text">
+                <h2>Welcome Back!</h2>
+                <p class="mt-5">Login into your account</p>
+
+                <form class="mt-4" ref="form" @submit.prevent="login">
+                  <v-text-field
+                    class="mt-15"
+                    type="email"
+                    v-model="email"
+                    name="email"
+                    label="Email"
+                    required
+                    filled
+                    dense
+                    :rules="emailRules"
+                    background-color="white"
+                  ></v-text-field>
+
+                  <v-text-field
+                    class="mt-5"
+                    :append-icon="showPassIcon ? 'mdi-eye' : 'mdi-eye-off'"
+                    :type="showPassIcon ? 'text' : 'password'"
+                    v-model="password"
+                    name="password"
+                    label="Password"
+                    hint="At least 6 characters"
+                    required
+                    filled
+                    dense
+                    @click:append="showPassIcon = !showPassIcon"
+                    :rules="passwordRules"
+                    background-color="white"
+                  ></v-text-field>
+
+                  <v-btn
+                    class="mt-5"
+                    color="warning"
+                    type="submit"
+                    width="83%"
+                    large
+                    >Login</v-btn
+                  >
+                </form>
+                <h5 class="mt-10">
+                  © Copyright 2022 Bacancy. All Right Reserved.
+                </h5>
+              </div>
+            </v-img>
           </v-col>
         </v-row>
       </v-container>
@@ -99,6 +160,10 @@ export default {
       emailRules: [
         (v) => !!v || "E-mail is required",
         (v) => /.+@.+\..+/.test(v) || "E-mail must be valid",
+      ],
+      passwordRules: [
+        (v) => !!v || "Password is required",
+        (v) => (v && v.length >= 6) || "password must be at least 6 characters",
       ],
       errorMessage: "",
       showPassIcon: false,
@@ -133,6 +198,20 @@ export default {
 </script>
 
 <style scoped>
+p {
+  font-size: 18px;
+}
+h1 {
+  font-size: 69px;
+}
+h2 {
+  font-size: 36px;
+}
+.v-text-field {
+  width: 400px;
+  margin-left: auto;
+  margin-right: auto;
+}
 @media all and (max-width: 600px) {
   .second__div {
     width: 30%;
